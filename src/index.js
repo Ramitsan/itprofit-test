017ee05a1e1886a29d4201d331f9bf13e2b19ff2
Ramitsan <ramitsan@gmail.com>
2023-10-27 01:27:13 +0300

    implement GET and POST requests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log("Hello World!");
+const baseUrl = 'http://localhost:9090/api';
+const REQUEST_STATUS_OK = 200;
+const REQUEST_STATUS_BAD = 400;
+const form = document.querySelector('.form');
+const successMessage = document.querySelector('.success');
+const errorMessage = document.querySelector('.error');
+
+fetch(`${baseUrl}/ping`, {
+  method: 'GET', 
+}).then(res => {
+  return res.json();
+})
+.then(data => console.log(data));
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  fetch(`${baseUrl}/registration`, {
+    method: 'POST', 
+    body: JSON.stringify({})
+  })
+  .then(res => {
+    return res.json().then(resData => {
+      return {
+        status: res.status,
+        data: resData
+      }
+    });
+  })
+  .then(response => {
+    console.log(response);
+    switch (response.status) {
+      case REQUEST_STATUS_OK:
+        onLoad(response.data);
+        break;
+      case REQUEST_STATUS_BAD:
+        onError(response.data);
+        break;
+      default:
+       console.log(response);
+    }
+  });
+})
+
+const onLoad = (data) => {
+  successMessage.style.display = '';
+  errorMessage.style.display = 'none';
+}
+
+const onError = (data) => {
+  successMessage.style.display = 'none';
+  errorMessage.style.display = '';
+}
