8f1eae7c8b6a75f3cef994ac62a44776c66879cf
Ramitsan <ramitsan@gmail.com>
2023-10-29 02:39:35 +0300

    move the request code into a separate module

--- src/index.js
+++ src/index.js
@@ -1,64 +1,18 @@
+import { getRequest, postRequest } from './js/ajax';
 import { isValidForm } from './js/validate';
 import { modalOpen} from './js/modal';
 import './scss/style.scss';
 
-const baseUrl = 'http://localhost:9090/api';
-const REQUEST_STATUS_OK = 200;
-const REQUEST_STATUS_BAD = 400;
 const form = document.querySelector('.form');
-const fields = form.querySelectorAll('input');
-const textarea = form.querySelector('textarea');
-const successMessage = document.querySelector('.message--success');
-const errorMessage = document.querySelector('.message--error');
 const modalButton = document.querySelector('.btn--modal');
 
-fetch(`${baseUrl}/ping`, {
-  method: 'GET', 
-}).then(res => {
-  return res.json();
-})
-.then(data => console.log(data));
+getRequest();
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const validRes = isValidForm();
   if(!validRes) return;
-  fetch(`${baseUrl}/registration`, {
-    method: 'POST', 
-    body: JSON.stringify({})
-  })
-  .then(res => {
-    return res.json().then(resData => {
-      return {
-        status: res.status,
-        data: resData
-      }
-    });
-  })
-  .then(response => {
-    switch (response.status) {
-      case REQUEST_STATUS_OK:
-        onLoad(response.data);
-        break;
-      case REQUEST_STATUS_BAD:
-        onError(response.data);
-        break;
-    }
-  });
+  postRequest();
 })
-
-const onLoad = (data) => {
-  fields.forEach(field => field.value = '');
-  textarea.value = '';
-  successMessage.textContent = data.message;
-  successMessage.classList.add('message-show');
-  errorMessage.style.display = 'none';
-}
-
-const onError = (data) => {
-  errorMessage.textContent = data.message;
-  errorMessage.classList.add('message-show');
-  errorMessage.style.display = '';
-}
 
 modalButton.addEventListener('click', modalOpen);
---
+++ src/js/ajax.js
@@ -0,0 +1,62 @@
+const baseUrl = 'http://localhost:9090/api';
+const requestStatusOK = 200;
+const requestStatusBad = 400;
+
+const form = document.querySelector('.form');
+const fields = form.querySelectorAll('input');
+const textarea = form.querySelector('textarea');
+
+const successMessage = document.querySelector('.message--success');
+const errorMessage = document.querySelector('.message--error');
+
+export const getRequest = () => {
+  fetch(`${baseUrl}/ping`, {
+    method: 'GET', 
+  })
+  .then(res => {
+    return res.json();
+  })
+  .then(data => console.log(data));
+}
+
+export const postRequest = () => {
+  fetch(`${baseUrl}/registration`, {
+    method: 'POST', 
+    body: JSON.stringify({})
+  })
+  .then(res => {
+    return res.json()
+    .then(resData => {
+      return {
+        status: res.status,
+        data: resData
+      }
+    });
+  })
+  .then(response => {
+    switch (response.status) {
+      case requestStatusOK:
+        onLoad(response.data);
+        break;
+      case requestStatusBad:
+        onError(response.data);
+        break;
+    }
+  });
+}
+
+const onLoad = (data) => {
+  fields.forEach(field => field.value = '');
+  textarea.value = '';
+  successMessage.textContent = data.message;
+  errorMessage.classList.remove('message-show');
+  successMessage.classList.add('message-show');
+}
+
+const onError = (data) => {
+  errorMessage.textContent = data.message;
+  successMessage.classList.remove('message-show');
+  errorMessage.classList.add('message-show');
+}
+
+
